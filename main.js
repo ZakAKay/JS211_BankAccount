15b1014e54a13faf681ff52a5f89417c9f21d576
ZakAKay <zakkay@ymail.com>
2022-07-17 21:30:00 -0500

    final version

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict'
+
+class BankAccount {
+    constructor(accountNumber, owner){
+            this.accountNumber =
+            this.owner = owner;
+            this.transaction = []
+    }
+
+}
+
+//tests below
+
+if (typeof des)
